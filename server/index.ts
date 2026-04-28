026c3157d0ab8b62cd04924b491601d7d056adc5
Prateek Bhardwaj <prateek.bhardwaj@alltius.ai>
2026-04-28 21:26:26 +0530

    create server

---
+++ server/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected:", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("server running on port 3001");
+});
